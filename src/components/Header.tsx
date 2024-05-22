db8943a5997945f750c0213eae32c518086113b0
boby900 <bobytiwari310@gmail.com>
2024-05-22 16:11:42 +0530

    adding the userProfile button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,12 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "./ui/mode-toggle";
 import {
-    HoverCard,
-    HoverCardContent,
-    HoverCardTrigger,
-  } from "@/components/ui/hover-card"
-  
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
+import { UserButton, SignInButton, SignedOut, SignedIn } from "@clerk/nextjs";
 
 export const Header = () => {
   return (
@@ -18,19 +18,26 @@ export const Header = () => {
         <div>
           <Input placeholder="Search for your products" />
         </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
         <div><Button>Login</Button></div>
         <div><Button>Cart</Button></div>
         <div><Button>Become a Seller</Button></div>
         <div><ModeToggle /></div>
+        <SignedIn>
+          <div> <UserButton /></div>
+
+        </SignedIn>
         <div>
-        <HoverCard>
-  <HoverCardTrigger>Hover</HoverCardTrigger>
-  <HoverCardContent>
-    The React Framework – created and maintained by @vercel.
-  </HoverCardContent>
-</HoverCard>
+          <HoverCard>
+            <HoverCardTrigger>Hover</HoverCardTrigger>
+            <HoverCardContent>
+              The React Framework – created and maintained by @vercel.
+            </HoverCardContent>
+          </HoverCard>
         </div>
-      
+
 
 
       </header>
