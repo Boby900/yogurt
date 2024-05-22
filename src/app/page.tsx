4f7238f5265c49908db982d34eb22b0782047a9f
boby900 <bobytiwari310@gmail.com>
2024-05-22 13:35:50 +0530

    did some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,10 @@ import React from 'react'
 import { Header } from '@/components/Header'
 const page = () => {
   return (
-<<<<<<< HEAD
-    <main>
-    bob
-    </main>
-  );
-=======
     <div>
       <Header />
     </div>
   )
->>>>>>> b6b37976fbfdd23cfd8472e0dd602e5757856636
 }
 
 export default page
