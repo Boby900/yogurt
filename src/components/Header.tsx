471ea2daa931afb13af31b013b9e7c5c825fdd0f
Boby900 <bobytiwari310@gmail.com>
2024-05-23 19:07:24 +0530

    rendering the data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,59 @@
+'use client'
 import React from "react";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./ui/mode-toggle";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
-} from "@/components/ui/hover-card"
-
+} from "@/components/ui/hover-card";
+import { useEffect, useState } from "react";
 import { UserButton, SignInButton, SignedOut, SignedIn } from "@clerk/nextjs";
 
 export const Header = () => {
+  const [images, setImages] = useState([]);
+ const getImages = () => {
+    fetch("/api/route")
+      .then((response) => response.json())
+      .then((data) => {console.log(data); setImages(data)})
+      .catch((error) => console.error("Error fetching data:", error));
+  };
   return (
     <div className=" h-[100px]">
       <header className="flex justify-around pt-4 bg-inherit">
-        <div><Button>Yogurt</Button></div>
+        <div>
+          <Button onClick={getImages}>Yogurt</Button>
+      
+        </div>
+        {images.map(image => (
+        <div key={image}>
+       
+          </div>))}
         <div>
           <Input placeholder="Search for your products" />
         </div>
         <div>
-
-
           <SignedOut>
-            <SignInButton><Button>Login</Button></SignInButton>
+            <SignInButton>
+              <Button>Login</Button>
+            </SignInButton>
           </SignedOut>
         </div>
-        <div><Button>Cart</Button></div>
-        <div><Button>Become a Seller</Button></div>
-        <div><ModeToggle /></div>
+        <div>
+          <Button>Cart</Button>
+        </div>
+        <div>
+          <Button>Become a Seller</Button>
+        </div>
+        <div>
+          <ModeToggle />
+        </div>
         <SignedIn>
-          <div> <UserButton /></div>
-
+          <div>
+            {" "}
+            <UserButton />
+          </div>
         </SignedIn>
         <div>
           <HoverCard>
@@ -40,9 +63,6 @@ export const Header = () => {
             </HoverCardContent>
           </HoverCard>
         </div>
-
-
-
       </header>
     </div>
   );
