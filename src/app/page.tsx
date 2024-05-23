5e69c8eda7574f515dcdaf1e49b7c8d3668bbab5
Boby900 <bobytiwari310@gmail.com>
2024-05-23 08:35:49 +0530

    feat: Add @radix-ui/react-avatar and embla-carousel-react dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Header } from '@/components/Header'
+import { CarouselDemo } from '@/components/CarouselDemo'
 const page = () => {
   return (
     <div>
       <Header />
+      <div className='justify-center items-center flex'>
+      < CarouselDemo />
+
+      </div>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button"
 import { ModeToggle } from "./ui/mode-toggle";
-import {
-    HoverCard,
-    HoverCardContent,
-    HoverCardTrigger,
-  } from "@/components/ui/hover-card"
-  
+import { DropdownMenuCheckboxes } from "./drop-down";
 
 
 export const Header = () => {
@@ -18,18 +13,12 @@ export const Header = () => {
         <div>
           <Input placeholder="Search for your products" />
         </div>
-        <div><Button>Login</Button></div>
+
+        <div><DropdownMenuCheckboxes /></div>
         <div><Button>Cart</Button></div>
         <div><Button>Become a Seller</Button></div>
         <div><ModeToggle /></div>
-        <div>
-        <HoverCard>
-  <HoverCardTrigger>Hover</HoverCardTrigger>
-  <HoverCardContent>
-    The React Framework – created and maintained by @vercel.
-  </HoverCardContent>
-</HoverCard>
-        </div>
+       
       
 
 
