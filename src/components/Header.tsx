c5f855c914d723623718adb248487cd4eba90392
Boby900 <bobytiwari310@gmail.com>
2024-05-25 10:09:31 +0530

    feat: Update dependencies and add lucide-react for SVG icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./ui/mode-toggle";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
 
+import {
+ 
+  Package2,
+ 
+} from "lucide-react";
 import { UserButton, SignInButton, SignedOut, SignedIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 export const Header = () => {
   
@@ -17,7 +18,10 @@ export const Header = () => {
     <div className=" h-[100px]">
       <header className="flex justify-around pt-4 bg-inherit">
         <div>
-          <Button>Yogurt</Button>
+        <Link href="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">Yogurt</span>
+            </Link>
           
         </div>
         <div><Input placeholder="Search for your products" /></div>
@@ -29,17 +33,16 @@ export const Header = () => {
           </SignedOut>
         </div>
         <div>
-          <Button>Cart</Button>
+          <Button variant={'outline'}><Link href="/upload">Inventory</Link> </Button>
         </div>
         <div>
-          <Button>Become a Seller</Button>
+          <Button variant={'outline'}>Become a Seller</Button>
         </div>
         <div>
           <ModeToggle />
         </div>
         <SignedIn>
           <div>
-            {" "}
             <UserButton />
           </div>
         </SignedIn>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher([
+  '/upload(.*)',
+  '/forum(.*)',
+]);
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
