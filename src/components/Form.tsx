6729f494f5e57f41ccaebc77418d5d7cb68654a8
Boby900 <bobytiwari310@gmail.com>
2024-05-26 07:37:53 +0530

    refactor: Update form component to include name field and submit button

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,12 +2,33 @@
 import { addItem } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useFormStatus } from "react-dom";
 
 export function ProfileForm() {
+  const [data, setData] = useState("");
+  function Sumbit() {
+    const { pending } = useFormStatus();
+    return (
+      <Button
+        type="submit"
+        disabled={pending}
+        onClick={() => setData("")}
+      >
+        {pending ? "Adding..." : "Add"}
+      </Button>
+    );
+  }
   return (
     <form action={addItem} className="space-y-8">
-      <Input placeholder="name" name="data" />
-      <Button type="submit">Submit</Button>
+      <Input
+        placeholder="name"
+        name="data"
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+      />
+      <Sumbit />
+      <div>{data}</div>
     </form>
   );
 }
