9ac61d55bdf76ae98879778284c7e5dd9a50fe1f
Boby900 <bobytiwari310@gmail.com>
2024-05-28 18:08:14 +0530

    refactor action.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,14 +5,14 @@ import postgres from 'postgres';
 export default async function Page(formData: FormData) {
  
     const sql = postgres(process.env.DATABASE_URL, { ssl: 'require' });
-    await sql`CREATE TABLE IF NOT EXISTS comments (
+    await sql`CREATE TABLE IF NOT EXISTS bob_bob (
       id SERIAL PRIMARY KEY,
-      comment TEXT
+      comments TEXT
     )`;
     const comment = formData.get("data");
     if (comment) {
       // Insert the comment into the table using parameterized query
-      await sql`INSERT INTO comments (comment) VALUES (${comment})`;
+      await sql`INSERT INTO bob_bob (comments) VALUES (${comment})`;
       console.log('Comment added successfully');
     } else {
       console.error('No data provided');
