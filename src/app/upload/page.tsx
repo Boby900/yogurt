4b0980a0d20330bd436f2b9d966e0c21ed90f130
Boby900 <bobytiwari310@gmail.com>
2024-06-01 16:29:05 +0530

    Update Form component to fix styling issues and use getPosts action

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -104,14 +104,14 @@ export default function page() {
             </div>
           </SignedIn>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <main className="flex flex-1 flex-col  gap-4 p-4 lg:gap-6 lg:p-6">
           <div className="flex items-center">
             <h1 className="text-lg font-semibold md:text-2xl">
               List your product
             </h1>
           </div>
           <div
-            className="flex flex-1  justify-center rounded-lg border border-dashed shadow-sm"
+            className="flex flex-1   justify-center rounded-lg border border-dashed shadow-sm"
             x-chunk="dashboard-02-chunk-1"
           >
             <ProfileForm />
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { getPosts } from "@/app/actions"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { getPosts } from "@/app/actions";
+import { Textarea } from "@/components/ui/textarea";
+
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,17 +15,16 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 const formSchema = z.object({
-  title:  z.string().min(2, {
+  title: z.string().min(2, {
     message: "Title must be at least 2 characters.",
   }),
-  content:  z.string().min(2, {
+  content: z.string().min(2, {
     message: "Content must be at least 2 characters.",
   }),
-  
-})
+});
 export type MyFormFields = z.infer<typeof formSchema>;
 export default function ProfileForm() {
   // 1. Define your form.
@@ -33,53 +34,56 @@ export default function ProfileForm() {
       title: "",
       content: "",
     },
-  })
+  });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    await getPosts(values)
-  
-  }
+    await getPosts(values);
+  };
   // 2. Define a submit handler.
-  
-  
-    return (
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit((data) => onSubmit(data))} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Title</FormLabel>
-                <FormControl>
-                  <Input placeholder="title" {...field} />
-                </FormControl>
-                <FormDescription>
-                  Please provide your title.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="content"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Content</FormLabel>
-                <FormControl>
-                  <Input placeholder="content" {...field} />
-                </FormControl>
-                <FormDescription>
-                  Please provide some content.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-    )
-  
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit((data) => onSubmit(data))}
+        className="space-y-4 w-[60%]"
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl >
+                <Input
+                 autoFocus={true}
+                  placeholder="title"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>Please provide your title.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Content</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us a little bit about yourself"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>Please provide some content.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
