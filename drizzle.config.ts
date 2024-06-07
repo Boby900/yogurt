033cf930ffeeaf9ed466d6d921720366b6150e62
Boby900 <bobytiwari310@gmail.com>
2024-06-07 06:20:42 +0530

    removed unwanted code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,3 @@
-// import { defineConfig } from "drizzle-kit";
-import { config } from "dotenv";
-
- config({ path: ".env.local" });
-
-// export default defineConfig({
-//   dialect: "postgresql",
-//   dbCredentials: {
-//     url: process.env.DATABASE_URL!
-//   },
-//   schema: "../yogurt/src/db/schema.ts",
-//   out: "./migrations",
-// });
-
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Header } from "@/components/Header";
-import { CarouselDemo } from "@/components/CarouselDemo";
 import Pricing from "@/components/Pricing";
 const page = () => {
   return (
