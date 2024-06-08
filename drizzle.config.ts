ba7ef47c5fce4ae208180d4a3bc9e454149fe517
Boby900 <bobytiwari310@gmail.com>
2024-06-08 08:18:01 +0530

    refactored the code

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "drizzle-kit";
-
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
---
+++ migrations/0004_acoustic_arclight.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "post_table" DROP COLUMN IF EXISTS "content";
