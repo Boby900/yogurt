e04fcd579a231220583928791056227fb2691df7
Boby900 <bobytiwari310@gmail.com>
2024-06-14 08:14:53 +0530

    chore: Add @testing-library/dom as a npm dependency

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Pricing from '@/components/Pricing'
-describe('Page', () => {
-  it('renders a heading', () => {
+describe('Pricing', () => {
+  it('renders a pricing', () => {
     render(<Pricing />)
  
     const heading = screen.getByRole('heading', { level: 1 })
