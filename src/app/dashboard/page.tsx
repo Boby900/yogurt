d1a0f41171802b3ce0ee1ef2447fa7c49c2d9a84
Boby900 <bobytiwari310@gmail.com>
2024-06-17 16:07:29 +0530

    chore: Update next.config.mjs with additional image hostnames

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,62 +1,122 @@
 import Image from "next/image";
-export default function page() {
+import { useEffect } from "react";
+export default async function page() {
   return (
     <div>
-      
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 m-2 p-2 lg:grid-cols-3">
-
-      
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <Image
-            src="https://plus.unsplash.com/premium_photo-1681469490747-8926a02f901e?q=80&w=1479&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Shoe"
-            width={200}
-            height={200}
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://plus.unsplash.com/premium_photo-1681469490747-8926a02f901e?q=80&w=1479&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body"></div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://plus.unsplash.com/premium_photo-1677093906029-1dbde84ba87c?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body"></div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://images.unsplash.com/photo-1562047823-3cab7fe719ee?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body">
+            <div className="card-actions justify-end"></div>
           </div>
         </div>
-      </div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <Image
-            src="https://plus.unsplash.com/premium_photo-1677093906029-1dbde84ba87c?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Shoe"
-            width={200}
-            height={200}
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://images.unsplash.com/photo-1598554747436-c9293d6a588f?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body">
+            <div className="card-actions justify-end"></div>
           </div>
         </div>
-      </div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <Image
-            src="https://images.unsplash.com/photo-1562047823-3cab7fe719ee?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Shoe"
-            width={200}
-            height={200}
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://images.unsplash.com/photo-1598554747436-c9293d6a588f?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body">
+            <div className="card-actions justify-end"></div>
           </div>
         </div>
-      </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://images.unsplash.com/photo-1598554747436-c9293d6a588f?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body">
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://images.unsplash.com/photo-1598554747436-c9293d6a588f?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body">
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://images.unsplash.com/photo-1566678124698-45c71a49eb0e?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body">
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <Image
+              src="https://images.unsplash.com/photo-1582552938357-32b906df40cb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt="Shoe"
+              width={200}
+              height={200}
+            />
+          </figure>
+          <div className="card-body">
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
