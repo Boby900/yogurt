b1e68a8087cb72568e9dacc65e6f832816835a8b
Boby900 <bobytiwari310@gmail.com>
2024-06-19 10:30:10 +0530

    refactor: Update page component and queries file

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,15 +1,14 @@
-
 import ProfileForm from "@/components/Form";
 export default function page() {
+  // Get the Backend API User object when you need access to the user's information
+
   return (
     <div>
-     
       <main className="flex flex-1 flex-col  gap-4 p-4 lg:gap-6 lg:p-6">
         <div
           className="flex flex-1   justify-center rounded-lg border border-dashed shadow-sm"
           x-chunk="dashboard-02-chunk-1"
-        > 
-        
+        >
           <ProfileForm />
         </div>
       </main>
--- src/db/queries.ts
+++ src/db/queries.ts
@@ -2,16 +2,19 @@
 import { db } from "@/db/db";
 import { postTable } from "@/db/schema";
 import { MyFormFields } from "@/components/Form";
+import { currentUser } from "@clerk/nextjs/server";
 
 export const addData = async (data: MyFormFields) => {
-  await db
-    .insert(postTable)
-    .values({
-      title: data.title,
-      content: data.content,
-    })
-   
+  const user = await currentUser();
+  if (user) {
+    console.log(user.id);
+  } else {
+    console.log("User is not signed in");
+  }
+  await db.insert(postTable).values({
+    title: data.title,
+    content: data.content,
+  });
+
   console.log("data added to db");
 };
-
-
