d9a1813b54e4b03af2c066205fcae0f9f4917183
Boby900 <bobytiwari310@gmail.com>
2024-06-30 07:37:34 +0530

    added one test.

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -4,5 +4,5 @@ import Home from '../src/app/page'
 
 test('Page', () => {
   render(<Home />)
-  expect(screen.getByRole('heading', { level: 1, name: `  The internet's coolest creator <br /> teams use Yogurt.` })).toBeDefined()
+  expect(screen.getAllByText(/yogurt/gi)).toBeDefined()
 })
